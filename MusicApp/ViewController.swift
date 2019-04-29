f26a5e4be4aa83abc97c5d8c38ca6ab13e6d559b
Dedi Dot <hello.dedidot@gmail.com>
2019-04-29 21:07:38 +0700

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Dedi Dot on 29/04/19.
+//  Copyright © 2019 Dedi Dot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
